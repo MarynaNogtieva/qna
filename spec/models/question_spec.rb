d893b8e4750d2049ac2f362e585433b9a02d301d
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2017-11-28 09:10:28 -0500

    Lesson-3 change context to describe

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  context 'ActiveRecord validations' do
+  describe 'associations'  do
     it { should have_many(:answers)}
     it { should have_many(:answers).dependent(:destroy)}
   end
   
-  context 'ActiveModel validations' do
+  describe 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:body) }
   end
