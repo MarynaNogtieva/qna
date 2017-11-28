e04ef6bf75f1cffbff2ed00d8e1fa43b6c0fc3ec
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2017-11-28 09:10:47 -0500

    re-written addition of foreign key

--- db/migrate/20171128024842_add_question_id_to_answers.rb
+++ db/migrate/20171128024842_add_question_id_to_answers.rb
@@ -1,6 +1,5 @@
 class AddQuestionIdToAnswers < ActiveRecord::Migration[5.1]
   def change
-    add_belongs_to :answers, :question
-    add_foreign_key :answers, :questions, on_delete: :cascade
+    add_belongs_to :answers, :question, foreign_key: { on_delete: :cascade }
   end
 end
