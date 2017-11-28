ad519ccc6dd4226ff40614a0850daadb675a408e
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2017-11-27 22:08:06 -0500

    add tests for answer

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'ActiveRecord validations' do
+    it { should belong_to(:question)}
+  end
+  
+  context 'ActiveModel validations' do
+    it { should validate_presence_of(:body) }
+  end
 end
