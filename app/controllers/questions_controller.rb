90cdbb2eda360a37a2acbc1681ab97cb29a66dad
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2017-12-04 22:11:35 -0500

    add question controller test

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,36 @@
 class QuestionsController < ApplicationController
+  before_action :load_question, only: [ :edit, :show ]
+  
   def index
     @questions = Question.all
   end
   
-  def show
+  def show; end
+  
+  def new
+    @question = Question.new
+  end
+  
+  def create
+    @question = Question.new(question_params)
+    @question.save
+    redirect_to @question
+    # if @question.save
+    #   redirect_to @question
+    # else
+    #   render :new
+    # end
+  end
+  
+  def edit; end
+  
+  private
+  
+  def load_question
     @question = Question.find(params[:id])
   end
   
-  
+  def question_params
+    params.require[:question].permit(:title, :body)
+  end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
-
+  let(:question) { create(:question) }
   
 
   describe 'GET #index' do
@@ -18,15 +18,57 @@ RSpec.describe QuestionsController, type: :controller do
   end
   
   describe 'GET #show' do
-    let(:question) { create(:question) }
-    
     before { get :show, params: { id: question } }
-    it 'assigns the requested question to question' do
+    
+    it 'assigns the requested question to @question' do
       expect(assigns(:question)).to eq question
     end
     
     it 'renders show view' do
       expect(response).to render_template :show
+    end
+  end
+  
+  describe 'Get #new' do
+    before { get :new }
+    
+    it 'assigns a new @uestion to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+    
+    it 'render new view' do
+      expect(response).to render_template :new
+    end
+  end
+  
+  describe 'Get #edit' do
+      before { get :edit, params: { id: question } }
+      
+      it 'assigns the requested question to @question' do
+        expect(assigns(:question)).to eq question
+      end
+      
+      it 'render new edit' do
+        expect(response).to render_template :edit
+      end
+  end
+  
+  describe 'Post #create' do
+    context'with valid attrs' do
+      it 'saves the new question in DB' do
+        # old_count = Question.count
+        # post :create, attributes_for(:question) #gets parameter hash from FactoryBot
+        # expect(Question.count).to eq old_count +1
+        expect { post :create, params: { question: attributes_for(:question) } }
+                .to change(Question, :count).by(1)
+      end
+      it 'redirects to show view' do
+          post :create, params: { question: attributes_for(:question) }
+          expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+    context'invalid object' do
+      
     end
   end
 end
