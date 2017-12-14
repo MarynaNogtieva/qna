b73e79e50555d95776472b0c6494e11b73b33906
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2017-12-13 22:06:22 -0500

    add acceptance test for user to sign in

---
+++ spec/acceptance/sign_in_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'User sign in', '
+    In order to be able to ask question
+    As an user
+    I want to be able to sign in
+' do
+
+  scenario 'Registered user tries to sign in' do
+    User.create!(email: 'user@test.com', password: '12345667')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345667' 
+    click_on 'Log in'
+
+    expect(page).to have_content 'Signed in successfully'
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'Non-registered user tries to sign in' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in 'Password', with: '1333345667'
+    click_on 'Log'
+
+    expect(page).to have_content 'Invalid Email or password. Log in Email Password Remember me Sign up Forgot your password?'
+    expect(current_path).to eq new_user_session_path
+  end
+end
