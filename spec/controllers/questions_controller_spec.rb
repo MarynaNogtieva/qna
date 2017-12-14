4f6f41236eafecfce3ba7141933a0031eee30fbe
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2017-12-13 23:09:28 -0500

    lesson-5 User tests  can create a question

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -30,6 +30,7 @@ RSpec.describe QuestionsController, type: :controller do
   end
   
   describe 'GET #new' do
+    sign_in_user
     before { get :new }
     
     it 'assigns a new @uestion to @question' do
@@ -42,18 +43,20 @@ RSpec.describe QuestionsController, type: :controller do
   end
   
   describe 'GET #edit' do
-      before { get :edit, params: { id: question } }
-      
-      it 'assigns the requested question to @question' do
-        expect(assigns(:question)).to eq question
-      end
-      
-      it 'render new edit' do
-        expect(response).to render_template :edit
-      end
+    sign_in_user
+    before { get :edit, params: { id: question } }
+    
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+    
+    it 'render new edit' do
+      expect(response).to render_template :edit
+    end
   end
   
   describe 'POST #create' do
+    sign_in_user
     context'with valid attrs' do
       it 'saves the new question in DB' do
         # old_count = Question.count
@@ -81,6 +84,7 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'PATCH #update' do
+    sign_in_user
     context'with valid attributess' do
       it 'assigns the requested question to @question'do
         patch :update, params: {id: question, question: attributes_for(:question)}
@@ -117,6 +121,7 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
+    sign_in_user
     before {question}
 
     it 'deletes question' do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -59,6 +59,9 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include Devise::Test::ControllerHelpers, type: :controller
   # config.include AcceptanceHelpers, type: :feature
+
+  #use extend because we use before blok, because before is used as class method in RSpec
+  config.extend ControllerMacros, type: :controller
 end
 
 
