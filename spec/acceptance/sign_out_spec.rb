18faec95b0260d7c700156dada1624c4ce7b7185
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2017-12-17 20:15:46 -0500

    lesson-5 add signout test

---
+++ spec/acceptance/sign_out_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'User sign out', '
+In order to finish my session on the website
+I as a user
+Want to be able to signout' do
+  given(:user) { create(:user) }
+  scenario 'Signed in user tries to logout' do
+    sign_in(user)
+    click_on 'Logout'
+
+    expect(page).to have_content 'Signed out successfully.'
+    expect(current_path).to eq '/'
+  end
+end
