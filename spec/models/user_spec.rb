9e3e28c8a0c1b766b80d68c7a87eb08bd4fa370b
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2017-12-20 18:33:28 -0500

    lesson-5 add author_of test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,22 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :password }
   it { should have_many(:questions).dependent(:destroy)}
   it { should have_many(:answers) }
+
+  describe '#author_of?' do
+    let (:user) { create(:user) }
+    let (:not_author_user) { create(:user) }
+    let (:question) { create(:question, user: user) }
+    
+    context 'user is an author of the question' do
+      it 'returns true' do
+        expect(user.author_of?(question)).to be true
+      end
+    end
+
+    context 'user is not an author of the question' do
+      it 'returns false' do
+        expect(not_author_user.author_of?(question)).to be false
+      end
+    end
+  end
 end
