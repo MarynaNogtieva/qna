201a430c6f2ad14db013253d46a5be419ca78ee2
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2017-12-20 18:33:53 -0500

    lesson-5 fix sign-in test

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -18,9 +18,9 @@ feature 'User sign in', '
     visit new_user_session_path
     fill_in 'Email', with: 'wrong@test.com'
     fill_in 'Password', with: '1333345667'
-    click_on 'Log'
-
-    expect(page).to have_content 'Invalid Email or password. Log in Email Password Remember me Sign up Forgot your password?'
+    click_on 'Log in'
+    
+    expect(page).to have_content 'Invalid Email or password.'
     expect(current_path).to eq new_user_session_path
   end
 end
