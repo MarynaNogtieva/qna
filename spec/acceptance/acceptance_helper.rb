4c246afb2890e2ec429c2b8fb0edb9be82cb653f
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2018-01-11 19:55:13 -0500

    lesson-7 fix transactional settings

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -2,12 +2,10 @@ require 'rails_helper'
 require 'capybara/poltergeist'
 
 RSpec.configure do |config|
-  
   Capybara.javascript_driver = :poltergeist
   Capybara.default_max_wait_time = 15
 
   config.include AcceptanceHelpers, type: :feature
-
   config.use_transactional_fixtures = false
 
   config.before(:suite) do
@@ -15,14 +13,18 @@ RSpec.configure do |config|
   end
 
   config.before(:each) do
-    DatabaseCleaner.strategy = :deletion
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each) do
     DatabaseCleaner.start
   end
 
-  config.before(:each) do
+  config.after(:each) do
     DatabaseCleaner.clean
   end
 end
