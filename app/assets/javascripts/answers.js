22f5d39f767bc707a3161841741b190b2459eff0
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2018-01-13 22:28:16 -0500

    lesson-7 fix answer js

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -1,15 +1,19 @@
 $(document).on('turbolinks:load',function() {
   $('.answers').on('click','.edit-answer-link', function(e) {
     e.preventDefault();
-    let answerId= $(this).data('answer-id');
-    $(this).hide();
+    let editLink = $(this);
+    let answerId=  editLink .data('answer-id');
+    editLink .hide();
     let answer = $('#edit-answer-'+answerId);
     answer.show();
+
     
+    $('.btn-save-updated-answer, .btn-cancel-edit-answer').on('click', function(e) {
+      e.preventDefault();
+      answer.hide();
+      editLink.show();
+    });
   });
-  $('.btn-save-updated-answer, .btn-cancel-edit-answer').on('click', function(e) {
-    e.preventDefault();
-    answer.hide();
-    $(this).show();
-  });
+  
+  
 });
