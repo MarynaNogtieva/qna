eebb3b4576e61dd5040d2fcabc9547feca2267fe
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2018-01-29 22:04:56 -0500

    lesson-8 accept nested attributes for attachements

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -11,4 +11,6 @@ RSpec.describe Question, type: :model do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:body) }
   end
+
+  it { should accept_nested_attributes_for :attachments}
 end
