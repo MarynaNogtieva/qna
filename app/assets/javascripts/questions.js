833641c8bd475addbfa27e7a9509ecf478b299b2
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2018-02-26 19:22:40 -0500

    lesson-9 refactor js

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,29 +1,3 @@
-  function ajaxRequest(method, url, data) {
-    var deferred = $.ajax({
-      url: url,
-      dataType: 'json',
-      contentType: 'application/json; charset=UTF-8',
-      type: method,
-      data: data ? JSON.stringify(data) : null
-    })
-    .then(onSuccess, onFail);
-
-    return deferred.promise();
-  }
-
-  function onSuccess(data){
-    return data;
-  }
-
-  function onFail(err){
-    return err
-  }
-
-  function toggleButtons(parent){
-    parent.find('.vote-for-btn').toggleClass('hide');
-    parent.find('.vote-against-btn').toggleClass('hide');
-    parent.find('.reset-vote-btn').toggleClass('hide');
-  }
 
 $(document).on('turbolinks:load',function() {
   $('.question').on('click','.edit-question-link', function(e) {
