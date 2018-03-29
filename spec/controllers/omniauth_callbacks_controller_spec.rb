1a42d2e8fdbdda162c497d755e710b1a24aae1b0
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2018-03-28 22:32:54 -0400

    add twitter test

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -39,4 +39,41 @@ RSpec.describe OmniauthCallbacksController, type: :controller do
       end
     end
   end
+
+
+  describe 'twitter' do
+    before do
+      request.env["devise.mapping"] = Devise.mappings[:user]
+      request.env["omniauth.auth"] = mock_auth_hash(:twitter, nil)
+      user.confirm
+    end
+
+    context 'new user' do
+      before { get :twitter }
+
+      it 'render confirm_email' do
+        expect(response).to render_template 'common/confirm_mail'
+      end
+
+      it 'doesnt create user' do
+        expect(controller.current_user).to eq nil
+      end
+    end
+
+    context 'exist user authenticate' do
+      before do
+        auth = mock_auth_hash(:twitter)
+        create(:authorization, user: user, provider: auth.provider, uid: auth.uid)
+        get :twitter
+      end
+
+      it 'redirects to root_path' do
+        expect(response).to redirect_to(root_path)
+      end
+
+      it 'doesnt create user' do
+        expect(controller.current_user).to eq user
+      end
+    end
+  end
 end
