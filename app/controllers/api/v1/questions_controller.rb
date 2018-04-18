7d9127024ff8597edf0e27a2100a0a17d1576100
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2018-04-18 09:09:18 -0400

    lesson-15 create questions API

---
+++ app/controllers/api/v1/questions_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::QuestionsController < Api::V1::BaseController
+  skip_authorization_check
+  def index
+    @questions = Question.all
+    respond_with @questions
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
       resources :profiles, only: :index do
         get :me, on: :collection
       end
+
+      resources :questions
     end
   end
 
