fc5793fc9bfa9005e7c1b0ead2378b39bd01580d
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2018-04-18 09:13:38 -0400

    lesson-15 authorize resource

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
-  skip_authorization_check
+  authorize_resource
   def index
     @questions = Question.all
     respond_with @questions
