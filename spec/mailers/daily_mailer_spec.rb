4fe55bd1a90e873beaa727eeb6bf96414a251403
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2018-06-10 22:40:26 -0400

    lesson-17 fix daily digest spec

--- spec/mailers/daily_mailer_spec.rb
+++ spec/mailers/daily_mailer_spec.rb
@@ -3,17 +3,16 @@ require "rails_helper"
 RSpec.describe DailyMailer, type: :mailer do
   describe "digest" do
     let(:user) { create(:user) }
-    let(:mail) { DailyMailer.digest(user) }
+    let(:questions) { create_list(:question, 5, user: user) }
+    let(:mail) { DailyMailer.digest(user, questions) }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Digest")
+    it "renders headers" do
       expect(mail.to).to eq([user.email])
       expect(mail.from).to eq(["from@example.com"])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it "renders body" do
+      expect(mail.body.encoded).to match("Here are questions for the last 24 hours")
     end
   end
-
 end
