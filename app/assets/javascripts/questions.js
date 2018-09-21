cbafbf1d4bfb296c198610c9eb6e038c2f4ca22c
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2018-09-20 23:20:17 -0400

    cahnge let to var for uglifier

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -2,10 +2,10 @@
 $(document).on('turbolinks:load',function() {
   $('.question').on('click','.edit-question-link', function(e) {
     e.preventDefault();
-    let editLink = $(this);
-    let questionId= editLink.data('question-id');
+    var editLink = $(this);
+    var questionId= editLink.data('question-id');
     editLink.hide();
-    let question = $('#edit-question-'+questionId);
+    var question = $('#edit-question-'+questionId);
     question.show();
 
     $('.btn-cancel-edit-question').on('click', function(e) {
