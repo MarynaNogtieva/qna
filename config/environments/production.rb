7c79d2b680210f5fd8871fffbfdc526c9a151d8b
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2018-09-21 00:24:33 -0400

    try to fix production for devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: '159.89.43.167' }
+  config.action_mailer.delivery_method = :smtp
 end
--- config/routes.rb
+++ config/routes.rb
@@ -48,4 +48,6 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
 
   get 'search', to: 'search#search'
+
+  default_url_options Rails.application.config.action_mailer.default_url_options
 end
